9ee6250adfbe97a9b527f0b0171e55b8b8b82b84
Jtobyy <jtobi8161@gmail.com>
2021-12-08 01:39:38 +0100

    set up basic settings

--- Laiyoweb/Laiyoweb/settings.py
+++ Laiyoweb/Laiyoweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIR_ROOT = BASE_DIR / 'media'
